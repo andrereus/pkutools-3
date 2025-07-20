75eb6e76ba8fe7cca1b809d1ad0a78c1cc636a96
Andre Reus <andrereus@users.noreply.github.com>
2025-07-20 18:10:52 +0200

    Install nuxt modules

--- README.md
+++ README.md
@@ -73,3 +73,7 @@ bun run preview
 ```
 
 Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
+
+## Notes
+
+- nuxt-headlessui is not v4 compatible yet
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    'nuxt-lucide-icons'
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'de', name: 'German', file: 'de.json' },
+      { code: 'es', name: 'Spanish', file: 'es.json' },
+      { code: 'fr', name: 'French', file: 'fr.json' }
+    ]
+  }
 })
